169ac81ea45ebf6a4e69ee84246661df84a11632
Andrey Lebedev <lebed3ff.4n@yandex.ru>
2022-04-07 21:57:27 +0200

    debager JWT

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -115,7 +115,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret',
         { expiresIn: '7d' },
       );
       res.cookie('jwt', token, {
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
