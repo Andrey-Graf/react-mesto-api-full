3cf24dced2d498e2b792c5ec2912b229ad74c716
Andrey Lebedev <lebed3ff.4n@yandex.ru>
2022-04-07 15:39:53 +0200

    debager secret_key

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -115,7 +115,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
       res.cookie('jwt', token, {
